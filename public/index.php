5f3def07dd9d311266177cc2ad6159a71c2b61c0
kurtco67 <48306773+kurtco67@users.noreply.github.com>
2019-05-03 13:44:11 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -52,7 +52,9 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
 $response->send();
 
