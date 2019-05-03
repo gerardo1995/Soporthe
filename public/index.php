45636776c3fe8c9611e168d8f6505b66472d6b3d
kurtco67 <48306773+kurtco67@users.noreply.github.com>
2019-05-03 13:44:02 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -23,6 +23,7 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
+
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
@@ -51,9 +52,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+
 
 $response->send();
 
